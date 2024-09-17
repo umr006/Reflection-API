67eb8e38e661fac24e66d7f410314849666cb31c
umr006 <umar.ausheff@yandex.ru>
2024-09-17 23:22:26 +0500

    ex00

--- src/ex00/main/java/edu/school21/Main.java
+++ src/ex00/main/java/edu/school21/Main.java
@@ -1,5 +1,7 @@
 package ex00.main.java.edu.school21;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.time.LocalDateTime;
 import java.util.Arrays;
@@ -13,10 +15,13 @@ import ex00.main.java.edu.school21.Classes.User;
 
 public class Main {
     public static Scanner scanner = new Scanner(System.in);
+    private static Object object;
+
     public static void main(String[] args) {
 
         Car car = null;
         User user = null;
+        Class clazz;
 
         System.out.println("Classes:");
         System.out.println("User");
@@ -25,21 +30,28 @@ public class Main {
         System.out.println("Enter class name");
         System.out.println("->");
         String className = scanner.nextLine();
-        if(className.equals("User")) {
-            Object object = user;
-            List<String> userFields = getFieldNames(User.class.getDeclaredFields());
-            List<String> userMethods = getMethodNames(User.class.getDeclaredMethods());
-            System.out.println("fields:");
-            userFields.forEach(f-> System.out.println("\t" + f));
-            System.out.println("methods:");
-            userMethods.forEach(m-> System.out.println("\t" + m));
-            System.out.println("----------------------------");
-            System.out.println("Let's create an object");
-            System.out.println("first name:" + "\n" + "->");
-            System.out.println("last name:" + "\n" + "->");
-            System.out.println("height:" + "\n" + "->");
-        }
 
+        try {
+            if(className.equals("User")) {
+                clazz = Class.forName(User.class.getName());
+                object = (User)clazz.newInstance();
+            } else {
+                clazz = Class.forName(Car.class.getName());
+                object = (Car)clazz.newInstance();
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        List<String> userFields = getFieldNames(object.getClass().getDeclaredFields());
+        List<String> userMethods = getMethodNames(object.getClass().getDeclaredMethods());
+        System.out.println("fields:");
+        userFields.forEach(f-> System.out.println("\t" + f));
+        System.out.println("methods:");
+        userMethods.forEach(m-> System.out.println("\t" + m));
+        System.out.println("----------------------------");
+        System.out.println("Let's create an object");
+        creatingObject();
+        changeField();
 
     }
 
@@ -54,5 +66,61 @@ public class Main {
                 .map(f -> f.getType().getSimpleName() + " " + f.getName())
                 .collect(Collectors.toList());
     }
+
+    private static void creatingObject() {
+        try {
+            Class<?> clazz = Class.forName(object.getClass().getName());
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field f : fields) {
+                System.out.println(f.getName() + ":" + "\n" + "->");
+                f.setAccessible(true);
+                setValue(f);
+            }
+            System.out.println("Object created:" + object);
+
+        } catch (Exception e ) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void setValue(Field field) throws IllegalAccessException {
+        switch (field.getType().getSimpleName()) {
+            case "int", "Integer" -> {
+                int value = scanner.nextInt();
+                field.setInt(object, value);
+                break;
+            }
+            case "String" -> {
+                String value = scanner.nextLine();
+                field.set(object, value);
+                break;
+            }
+            case "double", "Double" -> {
+                Double value = scanner.nextDouble();
+                field.set(object, value);
+                break;
+            }
+        }
+    }
+
+    private static void changeField() {
+        try {
+            System.out.println("Enter name if the field for changing:" + "\n" + "->");
+            String changeField = scanner.nextLine();
+            Field field = object.getClass().getDeclaredField(changeField);
+            field.setAccessible(true);
+            System.out.println("Enter " + field.getType().getSimpleName() + " value");
+            if (field.getType().getSimpleName().equals("String")) {
+                field.set(object, scanner.nextLine());
+            } else if (field.getType().getSimpleName().equals("int")) {
+                field.set(object, scanner.nextInt());
+            } else {
+                field.set(object, scanner.nextDouble());
+            }
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 }
