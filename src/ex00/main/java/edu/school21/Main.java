def5e47757f92d231e807d6df7358d970a114aaa
debian <debian@localhost.localdomain>
2024-09-18 11:46:34 +0500

    ex00

--- src/ex00/main/java/edu/school21/Main.java
+++ src/ex00/main/java/edu/school21/Main.java
@@ -18,11 +18,9 @@ public class Main {
     private static Object object;
 
     public static void main(String[] args) {
-
         Car car = null;
         User user = null;
         Class clazz;
-
         System.out.println("Classes:");
         System.out.println("User");
         System.out.println("Car");
@@ -35,49 +33,53 @@ public class Main {
             if(className.equals("User")) {
                 clazz = Class.forName(User.class.getName());
                 object = (User)clazz.newInstance();
-            } else {
+            } else if (className.equals("Car")) {
                 clazz = Class.forName(Car.class.getName());
                 object = (Car)clazz.newInstance();
+            } else {
+                throw new ClassNotFoundException();
             }
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
-        List<String> userFields = getFieldNames(object.getClass().getDeclaredFields());
-        List<String> userMethods = getMethodNames(object.getClass().getDeclaredMethods());
-        System.out.println("fields:");
-        userFields.forEach(f-> System.out.println("\t" + f));
-        System.out.println("methods:");
-        userMethods.forEach(m-> System.out.println("\t" + m));
-        System.out.println("----------------------------");
-        System.out.println("Let's create an object");
+        getFieldNames(object.getClass().getDeclaredFields());
+        getMethodNames(object.getClass().getDeclaredMethods());
         creatingObject();
         changeField();
+        callsMethod();
 
     }
 
-    private static List<String> getMethodNames (Method[] methods) {
-        return Arrays.stream(methods)
+    private static void getMethodNames (Method[] methods) {
+        List<String> userMethods =  Arrays.stream(methods)
                 .map(m -> m.getReturnType().getSimpleName() + " " + m.getName() + "(" + m.getReturnType().getSimpleName() + ")")
                 .collect(Collectors.toList());
+        System.out.println("methods:");
+        userMethods.forEach(m-> System.out.println("\t" + m));
     }
 
-    private static List<String> getFieldNames (Field[] fields) {
-        return Arrays.stream(fields)
+    private static void getFieldNames (Field[] fields) {
+        List<String> userFields =  Arrays.stream(fields)
                 .map(f -> f.getType().getSimpleName() + " " + f.getName())
                 .collect(Collectors.toList());
+        System.out.println("fields:");
+        userFields.forEach(f-> System.out.println("\t" + f));
+        System.out.println("----------------------------");
     }
 
     private static void creatingObject() {
         try {
+            System.out.println("Let's create an object");
             Class<?> clazz = Class.forName(object.getClass().getName());
             Field[] fields = clazz.getDeclaredFields();
             for (Field f : fields) {
                 System.out.println(f.getName() + ":" + "\n" + "->");
                 f.setAccessible(true);
                 setValue(f);
+                f.setAccessible(false);
             }
             System.out.println("Object created:" + object);
-
+            System.out.println("----------------------------");
         } catch (Exception e ) {
             e.printStackTrace();
         }
@@ -105,7 +107,8 @@ public class Main {
 
     private static void changeField() {
         try {
-            System.out.println("Enter name if the field for changing:" + "\n" + "->");
+            System.out.println("Enter name of the field for changing");
+            scanner.skip("\n");
             String changeField = scanner.nextLine();
             Field field = object.getClass().getDeclaredField(changeField);
             field.setAccessible(true);
@@ -117,10 +120,35 @@ public class Main {
             } else {
                 field.set(object, scanner.nextDouble());
             }
+            field.setAccessible(false);
+            System.out.println("Object updated:" + object);
+            System.out.println("----------------------------");
         } catch (NoSuchFieldException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
     }
 
-
+    private static void callsMethod() {
+        try {
+            System.out.println("Enter name of the method for call:");
+            String methodForCall = scanner.nextLine();
+            Method[] methods = object.getClass().getMethods();
+            Method targetMethod = null;
+            for (Method m : methods) {
+                if (m.toString().substring(m.toString().lastIndexOf(".") + 1).equals(methodForCall)) {
+                    targetMethod = m;
+                    break;
+                }
+            }
+            if (targetMethod == null) {
+                throw new NoSuchMethodException();
+            }
+            System.out.println("Enter" + targetMethod.getReturnType().getSimpleName() + "value" + "\n" + "->");
+            int newValue = scanner.nextInt();
+            targetMethod.setAccessible(true);
+            System.out.println("Method returned: " + targetMethod.invoke(object, newValue));
+        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
